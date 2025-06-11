6cad54e9999f137fd7d7fae1ade1e0dbcb8a09d0
Fabian van Hummel <fabianvanhummel@gmail.com>
2025-06-11 15:07:24 +0200

    first v with background

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Splashscreen } from './components/splashscreen/splashscreen';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Splashscreen],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'RujiraPortal';
